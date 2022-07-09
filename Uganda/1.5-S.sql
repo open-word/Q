4c323426dac2bc67b1740a6ba4d34ad0e69c525e
open-word <106508558+open-word@users.noreply.github.com>
2022-07-09 16:23:41 +0300

    Add PA.

--- Uganda/1.5-S.sql
+++ Uganda/1.5-S.sql
@@ -5,15 +5,19 @@ create Table S
 (
 	SectorID int,
 	Name nvarchar(7),
-	Reference nvarchar(10),
-	ReferenceName as Reference + '-' + Name,
-	constraint PK_S primary key (SectorID)
+	ShortName nvarchar(3),
+	Reference as format(SectorID,'D2'),
+	ReferenceName as format(SectorID,'D2') + ' - ' + Name,
+	ReferenceShortName as format(SectorID,'D2') + ' - ' + ShortName,
+	constraint PK_S primary key (SectorID),
+	constraint UQ_S_Name unique (Name),
+	constraint UQ_S_ShortName unique (ShortName)
 );
 go
 
-insert S (SectorID, Name, Reference) values
-	(1,'Public',	'01'),
-	(2,'Private',	'02');
+insert S (SectorID, Name, ShortName) values
+(1,	'Public',	'Pbl'),
+(2,	'Private',	'Prv');
 
 --select * from S;
 
